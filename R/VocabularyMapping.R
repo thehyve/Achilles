ea8c2ac6975f93fc1e49a5505f3cb5a8b22b1aa6
Maxim Moinat <maxim@thehyve.nl>
2017-10-02 11:49:52 +0200

    refactored top(un)mapped functions and added documentation

--- R/VocabularyMapping.R
+++ R/VocabularyMapping.R
@@ -1,19 +1,98 @@
-loadRenderTranslateSql2 <- function (sqlFilename, packageName, dbms = "sql server", ..., 
-          oracleTempSchema = NULL) 
-{
-  pathToSql <- system.file(paste("sql/", gsub(" ", "_", dbms), 
-                                 sep = ""), sqlFilename, package = packageName)
-  mustTranslate <- !file.exists(pathToSql)
-  if (mustTranslate) {
-    pathToSql <- system.file(paste("sql/", "sql_server", 
-                                   sep = ""), sqlFilename, package = packageName)
-  }
-  parameterizedSql <- readChar(pathToSql, file.info(pathToSql)$size)
-  renderedSql <- renderSql(parameterizedSql[1], ...)$sql
-  if (mustTranslate) 
-    renderedSql <- translateSql(sql = renderedSql, targetDialect = dbms, 
-                                oracleTempSchema = oracleTempSchema)$sql
-  renderedSql
+#' @title vocabularyMapping
+#'
+#' @description
+#' \code{vocabularyMapping} creates mapping statistics for a specific source vocabulary
+#'
+#' @details
+#' \code{vocabularyMapping} creates mapping statistics for a specific source vocabulary
+#' 
+#' @param connectionDetails An R object of type ConnectionDetail (details for the function that contains server info, database type, optionally username/password, port)
+#' @param cdmTable Name of cdm domain table to inspect, one of `condition_occurrence`, `procedure_occurrence`, `drug_exposure`, `measurement` or `observation`.
+#' @param sourceVocabularyId  Name of the source vocabulary to evaluate
+#' 
+#' @return A dataframe with results
+#' 
+#' @export
+vocabularyMapping <- function(connectionDetails, cdmTable, sourceVocabularyId){
+  sourceValueColumn = getMainSourceValueColumnName(cdmTable)
+
+  sql <- loadRenderTranslateSql2("vocabulary_mapping/AchillesMapping_v5.sql",
+                                 packageName = "Achilles",
+                                 dbms = connectionDetails$dbms,
+                                 cdm_schema = connectionDetails$schema,
+                                 cdm_table = cdmTable,
+                                 concept_source_column = sourceValueColumn,
+                                 source_vocabulary_id = sourceVocabularyId
+  )
+
+  connection <- connect(connectionDetails)
+  result_df <- querySql(connection, sql)
+
+  return(result_df)
+}
+
+#' @title Top Concepts Mapped
+#'
+#' @description
+#' \code{topMapped} returns a dataframe of the \code{topX} mapped concepts, based on occurrence
+#'
+#' @details
+#' \code{topMapped} returns a dataframe of the \code{topX} mapped concepts, based on occurrence
+#' 
+#' @param connectionDetails An R object of type ConnectionDetail (details for the function that contains server info, database type, optionally username/password, port)
+#' @param cdmTable Name of cdm domain table to inspect, one of `condition_occurrence`, `procedure_occurrence`, `drug_exposure`, `measurement` or `observation`.
+#' @param sourceVocabularyId  Name of the source vocabulary to evaluate
+#' @param topX
+#' 
+#' @return A dataframe with results
+#' 
+#' @export
+topMapped <- function(connectionDetails, cdmTable, sourceVocabularyId, topX = 10){
+  return(topSourceToTargetConcepts_(connectionDetails, cdmTable, sourceVocabularyId, topX, TRUE))
+}
+
+#' @title Top Concepts Not Mapped
+#'
+#' @description
+#' \code{topMapped} returns a dataframe of the \code{topX} unmapped concepts, based on occurrence
+#'
+#' @details
+#' \code{topMapped} returns a dataframe of the \code{topX} unmapped concepts, based on occurrence
+#' 
+#' @param connectionDetails An R object of type ConnectionDetail (details for the function that contains server info, database type, optionally username/password, port)
+#' @param cdmTable Name of cdm domain table to inspect, one of `condition_occurrence`, `procedure_occurrence`, `drug_exposure`, `measurement` or `observation`.
+#' @param sourceVocabularyId  Name of the source vocabulary to evaluate
+#' @param topX
+#' 
+#' @return A dataframe with the results
+#' 
+#' @export
+topUnmapped <- function(connectionDetails, cdmTable, sourceVocabularyId, topX = 10){
+  return(topSourceToTargetConcepts_(connectionDetails, cdmTable, sourceVocabularyId, topX, FALSE))
+}
+
+### Supportive functions ###
+
+topSourceToTargetConcepts_ <- function(connectionDetails, cdmTable, sourceVocabularyId, topX, getMapped) {
+  sourceValueColumn = getMainSourceValueColumnName(cdmTable)
+  conceptIdColumn = getMainConceptIdColumnName(cdmTable)
+  
+  ## Get top x mapped or unmapped concepts
+  sql <- loadRenderTranslateSql2("vocabulary_mapping/TopConcepts_v5.sql",
+                                 packageName = "Achilles",
+                                 dbms = connectionDetails$dbms,
+                                 cdm_schema = connectionDetails$schema,
+                                 cdm_table = cdmTable,
+                                 concept_source_column = sourceValueColumn,
+                                 concept_id_column = conceptIdColumn,
+                                 source_vocabulary_id = sourceVocabularyId,
+                                 get_mapped = getMapped,
+                                 limit = topX
+  )
+  connection <- connect(connectionDetails)
+  result_df <- querySql(connection, sql)
+  
+  return(result_df)
 }
 
 getMainSourceValueColumnName <- function(cdmTable) {
@@ -60,65 +139,20 @@ getMainConceptIdColumnName <- function(cdmTable) {
   }
 }
 
-#'
-vocabularyMapping <- function(connectionDetails, cdmTable, sourceVocabularyId){
-  sourceValueColumn = getMainSourceValueColumnName(cdmTable)
-
-  sql <- loadRenderTranslateSql2("vocabulary_mapping/AchillesMapping_v5.sql",
-                                 packageName = "Achilles",
-                                 dbms = connectionDetails$dbms,
-                                 cdm_schema = connectionDetails$schema,
-                                 cdm_table = cdmTable,
-                                 concept_source_column = sourceValueColumn,
-                                 source_vocabulary_id = sourceVocabularyId
-  )
-
-  connection <- connect(connectionDetails)
-  result_df <- querySql(connection, sql)
-
-  return(result_df)
-}
-
-#'
-topUnmapped <- function(connectionDetails, cdmTable, sourceVocabularyId, topX = 10){
-  sourceValueColumn = getMainSourceValueColumnName(cdmTable)
-  conceptIdColumn = getMainConceptIdColumnName(cdmTable)
-  
-  ## Get top unmapped
-  sql <- loadRenderTranslateSql2("vocabulary_mapping/TopUnmapped_v5.sql",
-                                 packageName = "Achilles",
-                                 dbms = connectionDetails$dbms,
-                                 cdm_schema = connectionDetails$schema,
-                                 cdm_table = cdmTable,
-                                 concept_source_column = sourceValueColumn,
-                                 concept_id_column = conceptIdColumn,
-                                 source_vocabulary_id = sourceVocabularyId,
-                                 limit = topX
-  )
-  connection <- connect(connectionDetails)
-  result_df <- querySql(connection, sql)
-  
-  return(result_df)
-}
-
-#'
-topMapped <- function(connectionDetails, cdmTable, sourceVocabularyId, topX = 10){
-  sourceValueColumn = getMainSourceValueColumnName(cdmTable)
-  conceptIdColumn = getMainConceptIdColumnName(cdmTable)
-  
-  ## Get top unmapped
-  sql <- loadRenderTranslateSql2("vocabulary_mapping/TopMapped_v5.sql",
-                                 packageName = "Achilles",
-                                 dbms = connectionDetails$dbms,
-                                 cdm_schema = connectionDetails$schema,
-                                 cdm_table = cdmTable,
-                                 concept_source_column = sourceValueColumn,
-                                 concept_id_column = conceptIdColumn,
-                                 source_vocabulary_id = sourceVocabularyId,
-                                 limit = topX
-  )
-  connection <- connect(connectionDetails)
-  result_df <- querySql(connection, sql)
-  
-  return(result_df)
+loadRenderTranslateSql2 <- function (sqlFilename, packageName, dbms = "sql server", ..., 
+                                     oracleTempSchema = NULL) 
+{
+  pathToSql <- system.file(paste("sql/", gsub(" ", "_", dbms), 
+                                 sep = ""), sqlFilename, package = packageName)
+  mustTranslate <- !file.exists(pathToSql)
+  if (mustTranslate) {
+    pathToSql <- system.file(paste("sql/", "sql_server", 
+                                   sep = ""), sqlFilename, package = packageName)
+  }
+  parameterizedSql <- readChar(pathToSql, file.info(pathToSql)$size)
+  renderedSql <- renderSql(parameterizedSql[1], ...)$sql
+  if (mustTranslate) 
+    renderedSql <- translateSql(sql = renderedSql, targetDialect = dbms, 
+                                oracleTempSchema = oracleTempSchema)$sql
+  renderedSql
 }
--- inst/sql/sql_server/vocabulary_mapping/TopConcepts_v5.sql
+++ inst/sql/sql_server/vocabulary_mapping/TopConcepts_v5.sql
@@ -11,11 +11,18 @@ SELECT
 	frequencies.source_code,
 	source_to_concept_map.source_vocabulary_id, 
 	source_to_concept_map.source_code_description,
+	concept.concept_name AS target_concept_name,
+	concept.concept_class_id AS target_concept_class_id,
 	frequency		
 FROM frequencies
 LEFT JOIN @cdm_schema.source_to_concept_map 
 	ON frequencies.source_code = source_to_concept_map.source_code
 	AND source_to_concept_map.source_vocabulary_id = '@source_vocabulary_id'
-WHERE (frequencies.target_concept_id = 0 OR frequencies.target_concept_id IS NULL)
+LEFT JOIN @cdm_schema.concept
+  ON concept.concept_id = frequencies.target_concept_id
+-- Get either mapped (with a target_concept_id) or unmapped (without target_concept_id)
+WHERE (@get_mapped AND frequencies.target_concept_id > 0) 
+      OR 
+      (NOT @get_mapped AND (frequencies.target_concept_id = 0 OR frequencies.target_concept_id IS NULL))
 ORDER BY frequency DESC
 LIMIT @limit
