85765806977aafc31476b940af9a5fbaf0c9f24b
Maxim Moinat <maxim@thehyve.nl>
2017-11-22 15:18:43 +0100

    mapping overview created if not exists yet. coverage for top (not) mapped concepts

--- R/VocabularyMapping.R
+++ R/VocabularyMapping.R
@@ -1,18 +1,18 @@
 #' @title Create table of all source to target concept mappings
 #'
 #' @description
-#' \code{createFullMappingOverview} creates and fills a table with concept mappings for all source/target pairs. These pairs can be found in \code{mappingFields.csv}.
+#' \code{createMappingOverview} creates and fills a table with concept mappings for all source/target pairs. These pairs can be found in \code{mappingFields.csv}.
 #' The \code{_source_vocabularies} are used to retrieve the \code{source_code_description} of \code{_source_value}'s. This gives more informative overviews of the top (not) mapped concepts.
 #'
 #' @details
-#' \code{createFullMappingOverview} creates and fills a table with concept mappings for all source/target pairs. These pairs can be found in \code{mappingFields.csv}.
+#' \code{createMappingOverview} creates and fills a table with concept mappings for all source/target pairs. These pairs can be found in \code{mappingFields.csv}.
 #' 
 #' @param connectionDetails An R object of type ConnectionDetail (details for the function that contains server info, database type, optionally username/password, port)
 #' @param resultsDatabaseSchema		string name of database schema that we can write results to. Default is 'webapi'
 #' @param condition_source_vocabularies		string or vector ids of the source vocabularies used to map condition_source_values to condition_concept_ids via the source_to_concept_map table.
 #' 
 #' @export
-createFullMappingOverview <- function(
+createMappingOverview <- function(
     connectionDetails, 
     resultsDatabaseSchema = "webapi",
     condition_source_vocabularies = "",
@@ -151,7 +151,12 @@ createMappingOverviewInsertQuery <- function(connectionDetails, resultsDatabaseS
 #'
 #' @export
 mappingStats <- function(connectionDetails, resultsDatabaseSchema = "webapi", mappingName = "") {
-  # TODO: check whether  createFullMappingOverview has been called. If not, rerun. Same for top[Not]Mapped method
+  connection <- connect(connectionDetails)
+  
+  # If no mapping overview created yet, do that here
+  if (!hasMappingOverview(connection, resultsDatabaseSchema, connectionDetails$dbms, mappingName)) {
+    createMappingOverview(connectionDetails, resultsDatabaseSchema)
+  }
 
   # Select everything if no mappingName is given
   if (mappingName == "" || is.null(mappingName)) {
@@ -165,7 +170,6 @@ mappingStats <- function(connectionDetails, resultsDatabaseSchema = "webapi", ma
                                  mapping_name = mappingName
   )
 
-  connection <- connect(connectionDetails)
   df <- querySql(connection, sql)
   
   df$COVERAGE <- df$FREQUENCY / sum(df$FREQUENCY) * 100
@@ -189,19 +193,32 @@ mappingStats <- function(connectionDetails, resultsDatabaseSchema = "webapi", ma
 #' 
 #' @export
 topMapped <- function(connectionDetails, resultsDatabaseSchema = "webapi", mappingName, topX = 20){
+  connection <- connect(connectionDetails)
+  # If no mapping overview created yet, do that here
+  if (!hasMappingOverview(connection, resultsDatabaseSchema, connectionDetails$dbms, mappingName)) {
+    createMappingOverview(connectionDetails, resultsDatabaseSchema)
+  }
+  
   if (is.null(topX)) {
     topX = "NULL"
   }
-  sql <- loadRenderTranslateSql2("vocabulary_mapping/TopMappedConcepts.sql",
-                                 packageName = "Achilles",
-                                 dbms = connectionDetails$dbms,
-                                 results_database_schema = resultsDatabaseSchema,
-                                 mapping_name = mappingName,
-                                 limit = topX
+  
+  sql <- loadRenderTranslateSql2(
+    "vocabulary_mapping/TopMappedConcepts.sql",
+    packageName = "Achilles",
+    dbms = connectionDetails$dbms,
+    results_database_schema = resultsDatabaseSchema,
+    mapping_name = mappingName,
+    limit = topX
   )
 
-  connection <- connect(connectionDetails)
-  return(querySql(connection, sql))
+  df <- querySql(connection, sql)
+  
+  # Coverage as percentage of all records in this mapping type
+  total <- getTotalFrequency(connection, resultsDatabaseSchema, connectionDetails$dbms, mappingName)
+  df$COVERAGE <- df$FREQUENCY / total * 100
+  
+  return(df)
 }
 
 #' @title Top Concepts Not Mapped
@@ -221,19 +238,65 @@ topMapped <- function(connectionDetails, resultsDatabaseSchema = "webapi", mappi
 #' 
 #' @export
 topNotMapped <- function(connectionDetails, resultsDatabaseSchema = "webapi", mappingName, topX = 20){
+  connection <- connect(connectionDetails)
+  # If no mapping overview created yet, do that here
+  if (!hasMappingOverview(connection, resultsDatabaseSchema, connectionDetails$dbms, mappingName)) {
+    createMappingOverview(connectionDetails, resultsDatabaseSchema)
+  }
+  
   if (is.null(topX)) {
     topX = "NULL"
   }
-  sql <- loadRenderTranslateSql2("vocabulary_mapping/TopNotMappedConcepts.sql",
-                                 packageName = "Achilles",
-                                 dbms = connectionDetails$dbms,
-                                 results_database_schema = resultsDatabaseSchema,
-                                 mapping_name = mappingName,
-                                 limit = topX
+  
+  sql <- loadRenderTranslateSql2(
+    "vocabulary_mapping/TopNotMappedConcepts.sql",
+    packageName = "Achilles",
+    dbms = connectionDetails$dbms,
+    results_database_schema = resultsDatabaseSchema,
+    mapping_name = mappingName,
+    limit = topX
   )
 
-  connection <- connect(connectionDetails)
-  return(querySql(connection, sql))
+  df <- querySql(connection, sql)
+  
+  # Coverage as percentage of all records in this mapping type
+  total <- getTotalFrequency(connection, resultsDatabaseSchema, connectionDetails$dbms, mappingName)
+  df$COVERAGE <- df$FREQUENCY / total * 100
+    
+  return(df)
+}
+
+#' @return numeric The total number of records for the given mappingName
+getTotalFrequency <- function(connection, resultsDatabaseSchema = "webapi", dbms, mappingName) {
+    if (mappingName == "" || is.null(mappingName)) {
+      mappingName = "%" 
+    }
+  
+    query <- SqlRender::translateSql(
+      sprintf("SELECT sum(frequency) AS TOTAL 
+              FROM %s.achilles_vocab_concept_mappings 
+              WHERE mapping_name LIKE '%s'",resultsDatabaseSchema, mappingName),
+      targetDialect=dbms
+    )
+    return(querySql(connection, query$sql)$TOTAL)
+}
+
+#' @return TRUE if \code{achilles_vocab_concept_mappings} contains records for the given mappingName. FALSE otherwise
+hasMappingOverview <- function(connection, resultsDatabaseSchema = "webapi", dbms,  mappingName = NULL) {
+    if (mappingName == "" || is.null(mappingName)) {
+      mappingName = "%" 
+    }
+  
+    query <- SqlRender::translateSql(
+      sprintf("SELECT count(*) AS COUNT 
+              FROM %s.achilles_vocab_concept_mappings 
+              WHERE mapping_name LIKE '%s'",resultsDatabaseSchema, mappingName),
+      targetDialect=dbms
+    )
+    
+    result <- querySql(connection, query$sql)$COUNT
+    
+    return(result > 0)
 }
 
 loadRenderTranslateSql2 <- function (sqlFilename, packageName, dbms = "sql server", ..., 
