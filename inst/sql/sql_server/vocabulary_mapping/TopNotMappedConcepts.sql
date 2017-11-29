ce86ba79fb2668431c8befc63a70dcd881745932
Maxim Moinat <maxim@thehyve.nl>
2017-11-29 18:14:01 +0100

    only relevant columns of top mapped an not mapped returned

--- inst/sql/sql_server/vocabulary_mapping/TopNotMappedConcepts.sql
+++ inst/sql/sql_server/vocabulary_mapping/TopNotMappedConcepts.sql
@@ -1,4 +1,10 @@
-SELECT *
+SELECT 
+  mapping_name,
+	source_code,
+	source_vocabulary_id,
+	source_code_description,
+	n_persons,
+	n_rows
 FROM @results_database_schema.achilles_vocab_concept_mappings
 WHERE mapping_name LIKE '@mapping_name' AND NOT is_mapped
 ORDER BY n_rows DESC
